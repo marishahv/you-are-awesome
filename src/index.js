4491eae51d35cc0740fc33bf9f7ffee51980b7fb
Marina <35458264+marishahv@users.noreply.github.com>
2018-03-14 16:33:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,63 @@
 // DO WHATEVER YOU WANT HERE
+/////////////////////////////////////////////////
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (propertyName) => {
+    Object.defineProperty(Object.prototype, propertyName, { 
+        __proto__: null,      
+        enumerable: true,
+        writable: true,   
+        configurable: true           
+    });
+    return propertyName;
+};
+
+/////////////////////////////////////////////////
+
+const createNotEnumerableProperty = function(propertyName){  
+    var propValue;  
+    Object.defineProperty(Object.prototype, propertyName, {         
+        configurable: true,     
+        get: () => {return propValue;},
+        set: (newValue) => {propValue = newValue;}                
+    });   
+    return propertyName;
+};
+
+/////////////////////////////////////////////////
+
+const createProtoMagicObject = () => { 
+    var myFunc = new Function;
+    myFunc.prototype = Function.prototype;
+    return myFunc;
+};
+
+///////////////////////////////////////////
+
+var count1 = 0;
+const incrementor = () => {
+    count1++;
+    return incrementor;       
+};
+incrementor.valueOf = () => count1;
+
+//////////////////////////////////////////
+
+var count2 = 0;
+const asyncIncrementor = async() => {
+    count2++;
+    return asyncIncrementor;
+};
+asyncIncrementor.valueOf = () => count2;
+
+//////////////////////////////////////////
+
+function* createIncrementer() {
+    var count = 1;
+    while(count < 20)
+      yield count++;
+}
+
+///////////////////////////////////////////
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
@@ -13,6 +65,7 @@ const getDeepPropertiesCount = () => {};
 const createSerializedObject = () => {};
 const toBuffer = () => {};
 const sortByProto = () => {};
+
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
